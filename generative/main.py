b1f5e1643c5b82ea1d7bc05421dc18f700f7ff37
yuvalkenneth <62839011+yuvalkenneth@users.noreply.github.com>
2023-05-10 20:46:30 +0300

    added dimension fix

--- generative/main.py
+++ generative/main.py
@@ -1,5 +1,5 @@
 import torch
-from torch.utils.data import DataLoader, TensorDataset
+from torch.utils.data import Dataset, TensorDataset
 from mingpt.model import GPT
 from mingpt.trainer import Trainer
 import mingpt.bpe
@@ -14,10 +14,22 @@ BLOCK_SIZE = 64
 LR = 5e-4
 
 
+class TrainSet(Dataset):
+    def __init__(self, tokens, labels):
+        self.tokens = tokens
+        self.labels = labels
+
+    def __len__(self):
+        return len(self.tokens)
+
+    def __getitem__(self, idx):
+        return self.tokens[idx], self.labels[idx]
+
+
 def init_model():
     model_config = GPT.get_default_config()
     model_config.model_type = 'gpt-nano'
-    model_config.vocab_size = VOCAB_SIZE  # TODO check if correct
+    model_config.vocab_size = VOCAB_SIZE
     model_config.block_size = BLOCK_SIZE
     gpt = GPT(model_config)
     return gpt
@@ -29,15 +41,21 @@ def init_trainer(model_to_train, data):
     train_config.max_iters = TRAIN_ITERATIONS
     train_config.batch_size = TRAIN_BATCH_SIZE
 
-    def batch_end_callback(trainer):
-        if trainer.iter_num % 100 == 0:
+    def batch_end_callback(train):
+        if train.iter_num % 100 == 0:
             print(
-                f"iter_dt {trainer.iter_dt * 1000:.2f}ms; iter {trainer.iter_num}: train loss "
-                f"{trainer.loss.item():.5f}")
+                f"iter_dt {train.iter_dt * 1000:.2f}ms; iter {train.iter_num}: train loss "
+                f"{train.loss.item():.5f}")
 
     trainer = Trainer(train_config, model_to_train, data)
     trainer.set_callback('on_batch_end', batch_end_callback)
     return trainer
+
+
+def data_by_blocks(data, block_size):
+    return [data[i:i + block_size] for i in range(len(data) - block_size - 1)], [data[i + block_size + 1]
+                                                                                 for i in range(
+            len(data) - block_size - 1)]
 
 
 if __name__ == '__main__':
@@ -47,11 +65,8 @@ if __name__ == '__main__':
     e = mingpt.bpe.BPETokenizer()
     tokenized_data = e(dataset)
 
-    x = torch.stack([tokenized_data[0][i:i + BLOCK_SIZE] for i in range(len(tokenized_data[0]) - BLOCK_SIZE -
-                                                                        1)])
-    y = torch.tensor([tokenized_data[0][i + BLOCK_SIZE + 1] for i in range(len(tokenized_data[0]) -
-                                                                           BLOCK_SIZE - 1)])
-    dataset = TensorDataset(x, y)
+    x, y = data_by_blocks(tokenized_data[0], BLOCK_SIZE)
+    dataset = TrainSet(x, y)
     model = init_model()
     model_trainer = init_trainer(model, dataset)
     model_trainer.run()
